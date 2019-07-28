fe422399c70e274923f6746357ea65486e677ccf
Shekar Mania <chandrasekhar.kohli@gmail.com>
2019-07-28 23:34:38 +0530

    graphql and root query and mutation

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            users: [String!]!
+        }
+        type RootMutation {
+            createUser(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      users: () => ['User 1', 'User 2', 'User 3'],
+      createUser: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  }),
+);
 
 app.listen(3000);
