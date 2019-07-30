aafe5abf15a81de4bba7ede38d3608e83d109b49
Shekar Mania <chandrasekhar.kohli@gmail.com>
2019-07-30 21:59:38 +0530

    Real time database creation and reading users

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const winston = require('winston');
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
 const app = express();
 
-const users = [];
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-management' },
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
 
 app.use(bodyParser.json());
 
@@ -18,12 +36,14 @@ app.use(
           username: String!
           password: String!
           contact: String!
+          email: String!
           date: String!
         }
         input UserInput {
           username: String!
           password: String!
           contact: String!
+          email: String!
           date: String!
         }
         type RootQuery {
@@ -38,21 +58,39 @@ app.use(
         }
     `),
     rootValue: {
-      users: () => users,
+      users: () => (
+        User.find()
+          .then(users => users.map(user => ({ ...user._doc, _id: user.id })))
+          .catch((err) => {
+            throw err;
+          })),
       createUser: (args) => {
-        const user = {
-          _id: Math.random().toString(),
+        const user = new User({
           username: args.userInput.username,
           password: args.userInput.password,
           contact: args.userInput.contact,
-          date: args.userInput.date,
-        };
-        users.push(user);
-        return user;
+          email: args.userInput.email,
+          date: new Date(args.userInput.date),
+        });
+        return user.save()
+          .then((result) => {
+            logger.info('user created successfully');
+            return { ...result._doc, _id: result.id };
+          })
+          .catch((err) => {
+            logger.error(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
   }),
 );
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@gql-dnwof.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    logger.error(err);
+  });
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
